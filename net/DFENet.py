26e3d75d1b1da3d784fb72feba452d962bd573e8
dadashuaiaaa <132824355+dadashuaiaaa@users.noreply.github.com>
2025-05-16 15:46:48 +0800

    Update DFENet.py

--- net/DFENet.py
+++ net/DFENet.py
@@ -14,7 +14,6 @@ def conv_layer(in_dim, out_dim, kernel_size=1, padding=0, stride=1):
         nn.Conv2d(in_dim, out_dim, kernel_size, stride, padding, bias=False),
         nn.BatchNorm2d(out_dim), nn.ReLU(True))
 
-
 def deconv_layer(in_dim, out_dim, kernel_size=1, padding=0, stride=1):
     return nn.Sequential(
         nn.ConvTranspose2d(in_dim, out_dim, kernel_size, stride, padding, bias=False),
